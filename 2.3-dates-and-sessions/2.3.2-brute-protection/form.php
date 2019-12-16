967372259fd8f02f67d777e8e2012ec0a19ec710
BRust1981 <42816397+BRust1981@users.noreply.github.com>
2019-12-16 09:23:55 +0300

    second commit 2.3-dates-and-sessions

--- 2.3-dates-and-sessions/2.3.2-brute-protection/form.php
+++ 2.3-dates-and-sessions/2.3.2-brute-protection/form.php
@@ -18,6 +18,20 @@
   <input type="text" name="login" placeholder="Логин" required>
   <input type="password" name="password" placeholder="Пароль" required>
   <button type="submit">Отправить</button>
+  <?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    if(isset($_COOKIE['bruteforcedetected'])){
+      echo '<script>
+        alert("Слишком часто вводите пароль. Попробуйте еще раз через минуту.");
+    </script>';
+    } elseif(isset($_COOKIE['loginsuccess'])) {
+      echo '<script>
+        alert("Успешная авторизация.");
+    </script>';
+    }
+  ?>
 </form>
+<!--<script src="form.js"></script>-->
 </body>
 </html>
--- 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
+++ 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
@@ -1,13 +1,56 @@
 <?php
+  ini_set('display_errors', 1);
+  error_reporting(E_ALL);
+
+  /*
+  Фиксировать в куках будем время ошибки и логироавть только данные с момента 
+  попытки взлома (третья попытка в 60 секунд считается брутом, ее и фиксируем)
+  */
   $users = [
-        'admin' => 'admin1234',
-        'randomUser' => 'somePassword',
-        'janitor' => 'nimbus2000'
-  ];
+          'admin' => 'admin1234',
+          'randomUser' => 'somePassword',
+          'janitor' => 'nimbus2000'
+    ];
+  
+  //если не зафиксирована попытка перебора
+  if(!isset($_COOKIE['bruteforcedetected'])){
+    //Если пришли значения login и password
+    if(isset($_POST['login']) && isset($_POST['password'])){
+      //если пришедшие значения логина-пароля совпали с существующими в системе
+      if(array_key_exists($_POST['login'], $users) && $users[$_POST['login']] == $_POST['password']){
+        $time = time();
+        setcookie('loginsuccess', $_POST['login'], $time);    //
+        setcookie('logintime', $time + 120, $time);    //
+        // иначе фиксируем ошибку
+      } else {
+        //фиксируем время
+        $curtime = time();
+        // если кука с ошибками уже заводилась
+        if(isset($_COOKIE['errortime1'])){
+          // если уже был второй случай ошибки
+          if(isset($_COOKIE['errortime2'])){
+            //если первая ошибка была менее минуты назад (о наличии второй говорит предыдущая проверка и она значит тоже входит в эту минуту)
+            if($_COOKIE['errortime1'] + 60 > $curtime){
+              setcookie('bruteforcedetected', true, time() + 86400);
+              //fixBrute
+            } else {
+              setcookie('errortime1', $_COOKIE['errortime2'], time() + 86400);
+              setcookie('errortime2', $curtime, time() + 86400);
+            }
+            $_COOKIE['errortime'] = $curtime;
+            $_COOKIE['errorcount'] = 1;
+          } else {
+            setcookie('errortime2', $curtime, time() + 86400);
+          }
+        } else {
+          setcookie('errortime1', $curtime, time() + 86400);
+        }
+        
+      }
+    } 
+  }
 
-  echo '123';
-  echo '<script>alert("error");</script>'
-
-  //header('HTTP/1.1 200 OK'); 
-  //header('Location: http://' . $_SERVER['HTTP_HOST'] . '/2.3-dates-and-sessions/2.3.2-brute-protection/form.html');
+  header('HTTP/1.1 200 OK'); 
+  header('Location: http://' . $_SERVER['HTTP_HOST'] . '/2.3-dates-and-sessions/2.3.2-brute-protection/form.php');
+  
 ?>

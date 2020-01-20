f690618757d956a367cfcf7e35df83aece3eed78
BRust1981 <42816397+BRust1981@users.noreply.github.com>
2020-01-20 23:05:50 +0300

    second commit 3.1-exceptions-and-headers

--- 3.1-exceptions-and-headers/3.1.1-router/index.php
+++ 3.1-exceptions-and-headers/3.1.1-router/index.php
@@ -2,30 +2,47 @@
   ini_set('display_errors', 1);
   error_reporting(E_ALL);
 
+  function checkParam($arr){
+    if(!empty($_GET) && !empty($_GET['page'])){
+
+      $pagename = $_GET['page'];
+  
+      if($arr->isAvailablePage($pagename)){
+        throw new Exception('(Not Found) Страница не найдена!', 404);
+      } else{
+        return $pagename;
+      }
+    } else {
+      throw new Exception('(Bad Request) Плохой запрос, очень...', 400);
+    }
+  }
+
   /**
  * Доступные страницы на сайте
  *
  * @var array $availableLinks
  */
   $availableLinks = include './availableLinks.php';
+
   require './router.php';
 
   $check = new Router($availableLinks);
 
-  if(!empty($_GET) && !empty($_GET['page'])){
+  
+try {
+  echo "Вы находитесь на странице <b>" . checkParam($check) . "</b>";
+}catch(Exception $e){
+  $errcode = $e->getCode();
+  header("HTTP/1.1 $errcode");
+  switch($errcode){
+    case 404:
+      header('Location: http://' . $_SERVER['HTTP_HOST'] . "/3.1-exceptions-and-headers/3.1.1-router/$errcode.php");
+      break;
+    default:
+      header('Location: http://' . $_SERVER['HTTP_HOST'] . '/3.1-exceptions-and-headers/3.1.1-router/error.php');
 
-    $pagename = $_GET['page'];
-
-    if($check->isAvailablePage($pagename)){
-      header('HTTP/1.1 404 Not Found'); 
-      header('Location: http://' . $_SERVER['HTTP_HOST'] . '/3.1-exceptions-and-headers/3.1.1-router/404.php');
-    } else{
-      echo "Вы находитесь на странице <b>$pagename</b>";
-    }
-
-  } else {
-    header('HTTP/1.1 400 Bad Request'); 
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . '/3.1-exceptions-and-headers/3.1.1-router/error.php');
   }
+}
+
 
 ?>
